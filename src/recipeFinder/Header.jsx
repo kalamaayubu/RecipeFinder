f0a0f95192510011d31f5ebc8812076b412e5031
Kalama Ayubu <kalamayeng@gmail.com>
2024-08-23 20:05:30 +0300

    Reciper Finder V.0.2

--- src/recipeFinder/Header.jsx
+++ src/recipeFinder/Header.jsx
@@ -1,11 +1,12 @@
 import SearchBar from "./SearchBar"
+import logo from '../assets/recipeImages/logo.png';
 
 
 function Header() {
 
   return (
     <div className="w-screen mx-auto px-4 flex justify-around align-center z-10">
-        <img src="src/assets/recipeImages/logo.png" alt="Logo" className="h-24 sm:h-28"/>
+        <img src={logo} alt="Logo" className="h-24 sm:h-28"/>
         <SearchBar/>
     </div>
   )
