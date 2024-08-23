430558412a005a81a265beffd9a1c80d4a644931
Kalama Ayubu <kalamayeng@gmail.com>
2024-08-23 20:21:13 +0300

    Recipe Finder V.0.3

--- src/recipeFinder/Footer.jsx
+++ src/recipeFinder/Footer.jsx
@@ -1,8 +1,9 @@
+import logo from '../assets/recipeImages/logo.png'
 
 function Footer() {
   return (
     <div className="bg-gray-50 w-full h-80 p-4 sm:p-10">
-        <img src="src/assets/recipeImages/logo.png" alt="Logo" className="h-24 sm:h-40"/>
+        <img src={logo} alt="Logo" className="h-24 sm:h-40"/>
     </div>
   )
 }
